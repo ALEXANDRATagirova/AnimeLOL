9bb3524d755c5e6d11bf8df0f7a4b9ac6f3b4f11
ALEXANDRATagirova <nastykat2333@gmail.com>
2025-10-21 16:21:50 +0700

    Delete AnimeSite-master directory

---
+++ ClassLibrary1/SimpleConfigModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using Model;
+using Ninject.Modules;
+
+namespace BusinessLogic
+{
+    public class SimpleConfigModule : NinjectModule
+    {
+        public override void Load()
+        {
+            // Регистрируем зависимость IRepository<Anime> -> EntityRepository<Anime> как Singleton
+            Bind<IRepository<Anime>>().To<EntityRepository<Anime>>().InSingletonScope();
+        }
+    }
+
+    // Дополнительный модуль для Dapper (если нужно сохранить выбор)
+    public class DapperConfigModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IRepository<Anime>>().To<DapperRepository<Anime>>().InSingletonScope();
+        }
+    }
+}
